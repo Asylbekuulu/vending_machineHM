2661516375033ec0caae58e635213baa411cae5a
Erzhan Asylbek uulu <erzhanasylbekuulu@gmail.com>
2025-09-19 10:56:24 +0600

    Initial commit - cloned repository to local server

--- src/model/CocaCola.java
+++ src/model/CocaCola.java
@@ -4,6 +4,7 @@ import enums.ActionLetter;
 
 public class CocaCola extends Product {
     public CocaCola(ActionLetter actionLetter, int price) {
+
         super("Coca Cola", actionLetter, price);
     }
 }
